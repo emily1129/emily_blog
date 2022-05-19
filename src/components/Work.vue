1f0aacbc51963aeddc1f5bdac87cc52c5ae8e4ad
emily <emilyvicki601@gmail.com>
2022-05-19 12:11:50 +0800

    feat: add headline connector

--- src/components/Work.vue
+++ src/components/Work.vue
@@ -3,6 +3,32 @@
     <p class="text-4xl text-left uppercase text-trueGray-700">
       work
     </p>
+    <!-- headline connector -->
+    <div class="flex mt-10">
+      <div class="w-1/8 pr-6 text-right border-r border-trueGray-300 timeline-block">
+        2021.12
+      </div>
+      <div class="w-5/6 ml-6">
+        <p class="text-2xl text-left uppercase text-trueGray-800">
+          Headline Connector
+        </p>
+        <div class="flex w-full md:flex-col mt-6">
+          <div class="w-1/2 mr-6 md:w-full leading-loose">
+            Headline Connector do data analysis based on both headline, or the topic you interest, just by simply type in the search line. To get the sense of the topic interesting you in a galance, we provide data analysis features, such as the text cloud based on your keyword, or the trend of the top youtubes. With this feature, it aims to expand your keyword to multiple approches, which are for you to explore further.
+            <div class="mt-6">
+              <a
+                href="https://fetapop.herokuapp.com/"
+                class="w-full p-1 text-white bg-orange-400 uppercase"
+              >link</a>
+            </div>
+          </div>
+          <img
+            src="@/assets/img/headline-connector.png"
+            class="w-1/2 object-contain lg:w-full shadow-xl"
+          >
+        </div>
+      </div>
+    </div>
     <!-- pickups -->
     <div class="flex mt-10">
       <div class="w-1/8 pr-6 text-right border-r border-trueGray-300 timeline-block">
@@ -13,7 +39,7 @@
           pick-ups
         </p>
         <div class="flex w-full md:flex-col mt-6">
-          <div class="w-1/2 pr-4 md:w-full leading-loose">
+          <div class="w-1/2 mr-6 md:w-full leading-loose">
             透過PICKUPS讓打球更容易<br>立即組隊 即刻開打 競爭較量 保存勝負<br>你，隨時準備上場<br>你，永遠知道下一場在哪
             <div class="mt-6">
               <a
@@ -39,7 +65,7 @@
           xmas
         </p>
         <div class="flex w-full md:flex-col mt-6">
-          <div class="w-1/2 pr-4 md:w-full leading-loose">
+          <div class="w-1/2 mr-6 md:w-full leading-loose">
             透過簡單的掃描 QR code 和答題小任務找到小精靈和獲得耶誕幣！累積的耶誕幣還能在新北耶誕城期間兌換抽獎資格
             <div class="mt-6">
               <a
@@ -65,7 +91,7 @@
           Reservamor Platform
         </p>
         <div class="flex w-full md:flex-col mt-6">
-          <div class="w-1/2 pr-4 md:w-full leading-loose">
+          <div class="w-1/2 mr-8 md:w-full leading-loose">
             Due to the COVID19, the city's Cultural Bureau decided to make the festival online. The web contains information like quotes from books and descriptions and images of the authors. The main purpose is to let the public to join the festival virtually when the world is facing the pandemic.
           </div>
           <img
